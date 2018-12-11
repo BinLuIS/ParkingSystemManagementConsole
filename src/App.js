a7e41cd8d1e2e33dcbd8bcccf9dbec7c886053b0
lamve2 <venice.lam@oocl.com>
2018-12-11 16:19:48 +0800

    [Venice] Remove unused route in App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import employeePage from './components/employeePage';
 import parkingLotPage from './components/parkingLotPage';
 import parkingClerkPage from './components/parkingClerkPage';
 import orderPage from './components/orderPage';
-import createParkingClerk from './components/createParkingClerk';
-import createParkingLot from './components/createParkingLot';
 import dashboardPage from './components/dashboardPage';
 
 const { Header, Sider, Content } = Layout;
@@ -88,8 +86,6 @@ class App extends Component {
               <Route path="/dashboardPage" component={dashboardPage}></Route>
               <Route path="/orderPage" component={orderPage}></Route> 
               <Route path="/nav3Page" component={()=><p style={{textAlign: 'center',marginTop:'15rem',color:'#1890ff', fontSize:'2rem'}}>Nav3 Page</p>}></Route>
-			        <Route path="/createParkingClerk" component={createParkingClerk}></Route>
-              <Route path="/createParkingLot" component={createParkingLot}></Route>
           </Switch>
           </Content>
         </Layout>
